2e864a30e4174466ac639022a60084fc497925f3
ggumiblv <gulnazhakimova70@gmail.com>
2024-10-05 12:39:34 +0200

    gh pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+'use client'
 import './globals.css';
 import { Provider } from 'react-redux';
 import { store } from '../store/store';
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
